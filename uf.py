69548201fe0467724291ff39aa3953cbe3eb40e9
shahak2 <47245335+shahak2@users.noreply.github.com>
2021-09-25 14:45:52 +0300

    Added animation and fixed issues
    An animation for creating the maze will now be shown.
    Fixed some implementation issues.

--- uf.py
+++ uf.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import random
+import sys
 
 n = 30
 width = 800
@@ -7,7 +8,7 @@ height = 600
 side = (height-100)/n
 
 #Union-find data structre using path compression and successive linking.
-class makeSet:
+class MakeSet:
     def __init__(self, key):
         self.key = key
         self.p = self
@@ -36,19 +37,22 @@ class makeSet:
 class Maze:
     def __init__(self):
     #Init a maze as a matrix of n x n size, where each cell is a set representing a cell in the maze
-        # and walls indicate up, right, down, left = True if they exist.
+    # and walls indicate up, right, down, left = True if they exist.
         self.board = []
         for i in range(n):
             boardRow = []
             for j in range(n):
-                boardRow.append([makeSet(i*n+j),True,True,True,True])
+                boardRow.append([MakeSet(i*n+j),True,True,True,True])
             self.board.append(boardRow)
-    
-    def build_maze(self):
+
+    def animate_build_maze(self, pg, background, screen):
         # Randomly removes edges until starting point and end point meet.
         self.board[0][0][1] = False
         self.board[n-1][n-1][3] = False
         while self.board[0][0][0].find() != self.board[n-1][n-1][0].find():
+            if game_events_handler(pg):
+                sys.exit(0)
+            background = reset_screen(background)
             row1 = random.randint(0,n-1)
             col1 = random.randint(0,n-1)
             row2, col2, pos = Maze.rand_neighbor(self, row1, col1)
@@ -62,76 +66,83 @@ class Maze:
                 elif pos == 4:
                     pos2 = 2
                 else:
-                    pos2 = 4                        
+                    pos2 = 4
                 self.board[row2][col2][pos2] = False
+            
+            self.blit_maze(background)
+            screen.blit(background, (0, 0))
+            pg.display.flip()
 
     def rand_neighbor(self, row, col):
+    # Returns a valod random neighbor of cell (row, col) in the matrix
+    # or none if none exist.
         arr = [(row-1,col), (row,col+1), (row+1,col), (row,col-1)]
         while True:
             i = random.randint(0,len(arr)-1)
-            if arr[i] != None:
+            if arr[i] is not None:
                 row2,col2 = arr[i]
                 if Maze.in_range(row2,col2):
                     return row2,col2, (i+1)
                 else:
                     arr[i] = None
         
-    def in_range(row,col):
+    def in_range(row, col):
         if row >= n or col >= n or row < 0 or col < 0:
             return False
         return True 
 
     def blit_maze(self, background):
-        current_pos = (100,50) 
-        
+        current_pos = (100,50)
         for row in self.board:
             current_pos = (150, current_pos[1])
             for col in row:
-                if col[1] == True:
+                if col[1]:
                     pg.draw.line(background, (0,0,0), current_pos,(current_pos[0] + side,current_pos[1]), 1)
-                if col[2] == True:
+                if col[2]:
                     pg.draw.line(background, (0,0,0), (current_pos[0] + side, current_pos[1]),(current_pos[0] + side,current_pos[1] + side), 1)
-                if col[3] == True:
+                if col[3]:
                     pg.draw.line(background, (0,0,0), (current_pos[0],current_pos[1] + side), (current_pos[0] + side,current_pos[1] + side), 1)
-                if col[4] == True:
+                if col[4]:
                     pg.draw.line(background, (0,0,0), current_pos, (current_pos[0],current_pos[1] + side), 1)              
                 current_pos = (current_pos[0] + side, current_pos[1])
-            current_pos = (current_pos[0] ,current_pos[1] + side)           
-            
+            current_pos = (current_pos[0] ,current_pos[1] + side)
         return background
-    
-def main():
-    m = Maze()
-    m.build_maze()
 
-    pg.init()
-    pg.display.set_caption('Union-Find Maze')
-    screen = pg.display.set_mode((width, height))
-
+def reset_screen(background):
     # Fill background
-    background = pg.Surface(screen.get_size())
     background = background.convert()
     background.fill((240, 240, 240))
-
-    # Display some text
+    # Display text
     font = pg.font.Font(None, 36)
     text = font.render("A-MAZE-ING", 1, (10, 10, 10))
     textpos = text.get_rect()
     textpos.centerx = background.get_rect().centerx
-    background.blit(text, textpos)
+    background.blit(text, (textpos[0], textpos[1] + 10))
+    return background
 
-    
-    # Event loop
-    while True:
-        for event in pg.event.get():
+def game_events_handler(pg):
+    for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
-                return
+                return True
+    else:
+        return False
 
-        m.blit_maze(background)        
 
-        screen.blit(background, (0, 0))
-        pg.display.flip()
+def main():
+    m = Maze()
+    pg.init()
+    pg.display.set_caption('Union-Find Maze')
+    screen = pg.display.set_mode((width, height))
+    background = pg.Surface(screen.get_size())
+    background = reset_screen(background)
+    m.animate_build_maze(pg, background, screen)
+
+    # Event loop
+    while True:
+        if game_events_handler(pg):
+            sys.exit(1)
+
 
 if __name__ == '__main__':
     main()
